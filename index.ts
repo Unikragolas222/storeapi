08c8695e34b9b91420f217d94f5ccd5464bedf0e
kragolas22 <kragolas22@gmail.com>
2025-12-04 11:21:18 +0000

    Store-API

--- index.ts
+++ index.ts
@@ -1,19 +1,15 @@
 import * as express from "express";
-import * as path from "path";
+import productRoutes from "./endpoints/products";
 
 const app = express();
-const port = parseInt(process.env.PORT) || process.argv[3] || 8080;
+const port = parseInt(process.env.PORT) || 8080;
 
-app.use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs');
+app.use(express.json());
+
+app.use('/api/products', productRoutes);
 
 app.get('/', (req, res) => {
-  res.render('index');
-});
-
-app.get('/api', (req, res) => {
-  res.json({"msg": "Hello world"});
+  res.send('HEAAAALLLLOOOOO');
 });
 
 app.listen(port, () => {
